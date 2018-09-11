c643fe0d987d4cecba1d7c40a2fb08288827793e
Curtis Rutland <crutland@amherst.com>
2018-09-11 17:48:22 -0500

    new clickable demo

---
+++ app/touchscreen/click-me.js
@@ -0,0 +1,42 @@
+/// <reference path="../types/pitft.d.ts" />
+
+var fb = require("pitft")("/dev/fb1", true);
+var touchscreen = require("pitft-touch");
+
+module.exports = function () {
+
+  console.log("starting node.js script...")
+
+  // Clear the back buffer
+  fb.clear();
+
+  var xMax = fb.size().width;
+  var yMax = fb.size().height;
+
+  var drawToCenter = function (text, invert) {
+    fb.clear();
+    if (invert) {
+      fb.color(0, 0, 0);
+    } else {
+      fb.color(1, 1, 1);
+    }
+    fb.rect(0, 0, xMax, yMax);
+    if (invert) {
+      fb.color(1, 1, 1);
+    } else {
+      fb.color(0, 0, 0);
+    }
+    fb.font("fantasy", 34);
+    fb.text(xMax / 2, yMax / 2, text, true);
+    fb.blit();
+  }
+
+  drawToCenter("Click Me", false);
+
+  var onTouch = function (err, data) {
+    var msg = "Touched at (" + data.x + "," + data.y + ")";
+    drawToCenter(msg, true);
+  }
+
+  touchscreen("/dev/input/touchscreen0", onTouch);
+}
