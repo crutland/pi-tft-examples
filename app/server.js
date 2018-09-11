18ad2999ae0e09e7b07223fd5f5e53af991d39b2
Curtis Rutland <crutland@amherst.com>
2018-09-11 16:16:29 -0500

    using multiple files

--- app/server.js
+++ app/server.js
@@ -1,93 +1,94 @@
 /// <reference path="../types/pitft.d.ts" />
 
-var touchscreen = require("pitft-touch");
-var fb = require("pitft")("/dev/fb1", true); // run 'npm install pitft' in the example directory
+var clock = require("./clock");
+clock();
 
-var gui = {};
+// var touchscreen = require("pitft-touch");
+// var fb = require("pitft")("/dev/fb1", true); // run 'npm install pitft' in the example directory
 
-for (var y = 0; y < 4; y++) {
-  for (var x = 0; x < 4; x++) {
-    gui["button_" + (y * 4 + x)] = {
-      "type": "button",
-      "pos": [x * 80 + 5, y * 60 + 5],
-      "size": [70, 50],
-      "background_color": [0, 0, 1],
-      "pressed_color": [1, 0, 1],
-      "text": {
-        "caption": "" + (y * 4 + x),
-        "font": ["fantasy", 24, true],
-        "color": [1, 1, 0]
-      },
-      "pressed": false
-    }
-  }
-}
+// var gui = {};
 
-var drawElement = function (element) {
-  if (gui[element].pressed) {
-    fb.color(gui[element].pressed_color[0], gui[element].pressed_color[1], gui[element].pressed_color[2]);
-  } else {
-    fb.color(gui[element].background_color[0], gui[element].background_color[1], gui[element].background_color[2]);
-  }
-  fb.rect(gui[element].pos[0], gui[element].pos[1], gui[element].size[0], gui[element].size[1], true);
-  fb.color(gui[element].text.color[0], gui[element].text.color[1], gui[element].text.color[2]);
-  fb.font(gui[element].text.font[0], gui[element].text.font[1], gui[element].text.font[2]);
-  fb.text(gui[element].pos[0] + gui[element].size[0] / 2, gui[element].pos[1] + gui[element].size[1] / 2, gui[element].text.caption, true);
-}
+// for (var y = 0; y < 4; y++) {
+//   for (var x = 0; x < 4; x++) {
+//     gui["button_" + (y * 4 + x)] = {
+//       "type": "button",
+//       "pos": [x * 80 + 5, y * 60 + 5],
+//       "size": [70, 50],
+//       "background_color": [0, 0, 1],
+//       "pressed_color": [1, 0, 1],
+//       "text": {
+//         "caption": "" + (y * 4 + x),
+//         "font": ["fantasy", 24, true],
+//         "color": [1, 1, 0]
+//       },
+//       "pressed": false
+//     }
+//   }
+// }
 
-var getElementUnderCursor = function (x, y) {
-  for (var element in gui) {
-    if (gui.hasOwnProperty(element)) {
-      if (x >= gui[element].pos[0] && x < (gui[element].pos[0] + gui[element].size[0])) {
-        if (y >= gui[element].pos[1] && y < (gui[element].pos[1] + gui[element].size[1])) {
-          return element;
-        }
-      }
-    }
-  }
+// var drawElement = function (element) {
+//   if (gui[element].pressed) {
+//     fb.color(gui[element].pressed_color[0], gui[element].pressed_color[1], gui[element].pressed_color[2]);
+//   } else {
+//     fb.color(gui[element].background_color[0], gui[element].background_color[1], gui[element].background_color[2]);
+//   }
+//   fb.rect(gui[element].pos[0], gui[element].pos[1], gui[element].size[0], gui[element].size[1], true);
+//   fb.color(gui[element].text.color[0], gui[element].text.color[1], gui[element].text.color[2]);
+//   fb.font(gui[element].text.font[0], gui[element].text.font[1], gui[element].text.font[2]);
+//   fb.text(gui[element].pos[0] + gui[element].size[0] / 2, gui[element].pos[1] + gui[element].size[1] / 2, gui[element].text.caption, true);
+// }
 
-  return null;
-}
+// var getElementUnderCursor = function (x, y) {
+//   for (var element in gui) {
+//     if (gui.hasOwnProperty(element)) {
+//       if (x >= gui[element].pos[0] && x < (gui[element].pos[0] + gui[element].size[0])) {
+//         if (y >= gui[element].pos[1] && y < (gui[element].pos[1] + gui[element].size[1])) {
+//           return element;
+//         }
+//       }
+//     }
+//   }
 
-fb.clear();
-for (var element in gui) {
-  if (gui.hasOwnProperty(element)) {
-    drawElement(element);
-  }
-}
-fb.blit();
+//   return null;
+// }
 
-var elementUnderCursor, pressedElement;
+// fb.clear();
+// for (var element in gui) {
+//   if (gui.hasOwnProperty(element)) {
+//     drawElement(element);
+//   }
+// }
+// fb.blit();
 
-touchscreen("/dev/input/touchscreen0", function (err, data) {
-  if (err) {
-    console.error(err);
-    throw err;
-  }
+// var elementUnderCursor, pressedElement;
 
-  var screenX = ((270 - 50) / (693 - 3307)) * data.x + 320;
-  var screenY = ((190 - 50) / (3220 - 996)) * data.y;
+// touchscreen("/dev/input/touchscreen0", function (err, data) {
+//   if (err) {
+//     console.error(err);
+//     throw err;
+//   }
 
-  if (pressedElement && data.touch == 0) {
-    gui[pressedElement].pressed = false;
-    drawElement(pressedElement);
-  }
+//   var screenX = ((270 - 50) / (693 - 3307)) * data.x + 320;
+//   var screenY = ((190 - 50) / (3220 - 996)) * data.y;
 
-  elementUnderCursor = getElementUnderCursor(screenX, screenY);
+//   if (pressedElement && data.touch == 0) {
+//     gui[pressedElement].pressed = false;
+//     drawElement(pressedElement);
+//   }
 
-  if (elementUnderCursor) {
-    if (data.touch == 1) {
-      pressedElement = elementUnderCursor;
-      gui[pressedElement].pressed = true;
-      drawElement(pressedElement);
-      console.log(gui[pressedElement].text.caption);
-    }
-  }
+//   elementUnderCursor = getElementUnderCursor(screenX, screenY);
 
-  fb.blit();
-});
+//   if (elementUnderCursor) {
+//     if (data.touch == 1) {
+//       pressedElement = elementUnderCursor;
+//       gui[pressedElement].pressed = true;
+//       drawElement(pressedElement);
+//       console.log(gui[pressedElement].text.caption);
+//     }
+//   }
 
-setInterval(function () { console.log("running...") }, 9000);
+//   fb.blit();
+// });
 
 // var fb = require("pitft")("/dev/fb1", true); // run 'npm install pitft' in the example directory
 // console.log("starting node.js script...")
