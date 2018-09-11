e4b84503dcfaa076cac0c8098440564fb8902071
Curtis Rutland <crutland@amherst.com>
2018-09-11 16:23:35 -0500

    testing initial code again

---
+++ app/display/hello-amherst.js
@@ -0,0 +1,26 @@
+var fb = require("pitft")("/dev/fb1", true);
+
+module.exports = function () {
+
+  console.log("starting node.js script...")
+
+  // Clear the back buffer
+  fb.clear();
+
+  var xMax = fb.size().width;
+  var yMax = fb.size().height;
+
+  var draw = function () {
+    fb.clear();
+    fb.color(0, 0, 0)
+    fb.rect(0, 0, xMax, yMax);
+    fb.color(1, 1, 1);
+    fb.font("fantasy", 34);
+    fb.text(xMax / 2, yMax / 2, "Hello Amherst!", true);
+    fb.blit();
+  }
+
+  draw();
+
+  setInterval(draw, 5000);
+}
