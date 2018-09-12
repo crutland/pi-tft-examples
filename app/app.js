cf9e491cd5c835d1c008c945151d1b9ccaa3bac4
Curtis Rutland <crutland@amherst.com>
2018-09-12 10:43:37 -0500

    updated dockerfile

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 //var app = require("./display/clock");
 //var app = require("./touchscreen/button-grid");
-var app = require("./display/hello-amherst");
-//var app = require("./touchscreen/click-me");
+//var app = require("./display/hello-amherst");
+var app = require("./touchscreen/click-me");
 
 app();
